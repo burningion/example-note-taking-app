95f25a1ab7e158e0fee48b4abc663f46b85b6a45
Kirk Kaiser <kirk@zothcorp.com>
2019-08-12 15:48:22 -0400

    More cleanup, base app exists

--- frontend/api.py
+++ frontend/api.py
@@ -18,15 +18,17 @@ app = Flask('api')
 
 if os.environ['FLASK_DEBUG']:
     CORS(app)
-    
-IMAGE_CLIP_URL = f"http://{environ['IMAGE_CLIP_SERVICE_HOST']}:{environ['IMAGE_CLIP_SERVICE_PORT_HTTP']}"
-NOTES_URL = f"http://{environ['NOTES_SERVICE_SERVICE_HOST']}:{environ['NOTES_SERVICE_SERVICE_PORT']}"
-AUTHENTICATION_URL = f"http://{environ['AUTHENTICATION_SERVICE_HOST']}:{environ['AUTHENTICATION_SERVICE_PORT_HTTP']}"
+
+# Uncomment below if working in Kubernetes
+  
+# IMAGE_CLIP_URL = f"http://{environ['IMAGE_CLIP_SERVICE_HOST']}:{environ['IMAGE_CLIP_SERVICE_PORT_HTTP']}"
+# NOTES_URL = f"http://{environ['NOTES_SERVICE_SERVICE_HOST']}:{environ['NOTES_SERVICE_SERVICE_PORT']}"
+# AUTHENTICATION_URL = f"http://{environ['AUTHENTICATION_SERVICE_HOST']}:{environ['AUTHENTICATION_SERVICE_PORT_HTTP']}"
 # GOLANG_URL = f"http://{environ['GO_CONCURRENT_SERVICE_SERVICE_HOST']}:{environ['GO_CONCURRENT_SERVICE_SERVICE_PORT']}"
 
-app.logger.info("Image Clip URL: " + IMAGE_CLIP_URL)
-app.logger.info("Notes URL: " + NOTES_URL)
-app.logger.info("Authentication URL: " + AUTHENTICATION_URL)
+# app.logger.info("Image Clip URL: " + IMAGE_CLIP_URL)
+# app.logger.info("Notes URL: " + NOTES_URL)
+# app.logger.info("Authentication URL: " + AUTHENTICATION_URL)
 
 @app.route('/')
 def homepage():
